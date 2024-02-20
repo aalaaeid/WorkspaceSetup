bc15ca924eac53ea1a79fd7b6755dd89bd058de7
Alaa <aalaaeiid@gmail.com>
2024-02-20 10:13:29 +0200

    E_Core iphone

---
+++ E_Core/E_Core/E_UIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  E_UIViewController.swift
+//  E_Core
+//
+//  Created by Alaa Eid on 20/02/2024.
+//
+
+import UIKit
+
+class E_UIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
